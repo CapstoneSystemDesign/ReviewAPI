e23bcbe6cda0ca68b88ca2164489f78ddadc9bfc
Jake Hansen <that1duder@Jakes-MBP.lan>
2022-04-24 21:32:06 -0500

    begins server and db models implementation.

---
+++ src/Reviews/index.js
@@ -0,0 +1,4 @@
+// Export system file?
+// I can now use import from statements throughout
+// the server files thannks to pkg json module statement
+// EXAMPLE: import logo from './logo.svg';
---
+++ src/Reviews/reviews.routes.js
@@ -0,0 +1,14 @@
+// import express from "express";
+// const router = express.Router();
+
+// import {
+//   all, show, create, update, destroy
+// } from "./movie.controller.js";
+
+// router.get("/", all);
+// router.get("/:id", show);
+// router.post("/", create);
+// router.put("/:id", update);
+// router.delete("/:id", destroy);
+
+// export default router;
